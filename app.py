9da08daad400c83f7a0c4fa158d0fa58023640f9
Jhair1122 <jhairurbina15@gmail.com>
2026-05-04 12:08:53 -0500

    Change default port from 10000 to 7860

--- app.py
+++ app.py
@@ -303,5 +303,5 @@ print("Antes de lanzar Gradio")
 
 if __name__ == "__main__":
     import os as _os
-    port = int(_os.environ.get("PORT", 10000))
+    port = int(_os.environ.get("PORT", 7860))
     demo.launch(server_name="0.0.0.0", server_port=port)
